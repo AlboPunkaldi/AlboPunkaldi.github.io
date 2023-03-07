173f992256a74aaa961bb1efa0589dcafc5c3496
Alberto Pancaldi <alberto.pancaldi95@gmail.com>
2023-03-07 22:44:46 +0100

    Preload large images

--- js/gallery.js
+++ js/gallery.js
@@ -9,6 +9,10 @@ function activateGallery() {
   let title       = galleryInfo.querySelector(".title");
   let description = galleryInfo.querySelector(".description");
   thumbnails.forEach(function(thumbnail) {
+      // Preload large images.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      let largeVersion = new Image();
+      largeVersion.src = newImageSrc;
     thumbnail.addEventListener("click", function() {
         // Set clicked image as main image.
         let newImageSrc = thumbnail.dataset.largeVersion;
